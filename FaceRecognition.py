8434221e8fbca9fce4847664ed1cd4413c0240c9
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-05-08 02:03:58 +0200

    Initialized

---
+++ FaceRecognition.py
@@ -0,0 +1,24 @@
+import face_recognition
+image = face_recognition.load_image_file("Images\Data\jpg")
+face_locations = face_recognition.face_locations(image)
+face_landmarks_list = face_recognition.face_landmarks(image)
+
+known_image = face_recognition.load_image_file("Ugur_DURA.jpg")
+
+face_locations=face_recognition.face_locations(image)
+
+print("I found {} face(s) in this photograph.".format(len(face_locations)))
+
+for face_location in face_locations:
+
+    # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+
+    # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
+
+
+
