d2a247cb852d3d61880e8cb0c2fe6308498b6e22
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-05-08 02:05:17 +0200

    Update FaceRecognition.py

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -1,5 +1,5 @@
 import face_recognition
-image = face_recognition.load_image_file("Images\Data\jpg")
+image = face_recognition.load_image_file(r"Images\Data\jpg")
 face_locations = face_recognition.face_locations(image)
 face_landmarks_list = face_recognition.face_landmarks(image)
 
