329082eb60d72de776384b161ef0d7c4d41b3233
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-05-08 03:59:17 +0200

    FaceIdentification Completed
    small pool

---
+++ FaceMatch.py
@@ -0,0 +1,16 @@
+import face_recognition
+
+image_of_Ugur=face_recognition.load_image_file('Images/Data/Known/Ugur_DURA.jpg')
+ugur_face_encoding=face_recognition.face_encodings(image_of_Ugur)[0]
+
+image_of_team=face_recognition.load_image_file('Images/Team1.jpeg')
+team_face_encoding=face_recognition.face_encodings(image_of_team)[0]
+
+
+results=face_recognition.compare_faces([ugur_face_encoding],team_face_encoding)
+
+if results[0]:
+    print('This is mathes')
+    
+else:
+    print('Nope')
